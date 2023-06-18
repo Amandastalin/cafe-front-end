a90dd4de7ee3cf2a001c322706a6e6acecebe17d
Rafael0365 <rafaelantonio0365@gmail.com>
2023-06-17 21:56:18 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'cafe-front',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
